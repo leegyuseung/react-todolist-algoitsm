66893d31dd10ca550a03ce66134750693fa9eed8
lee gyuseung <gyus0421@gmail.com>
2024-01-10 14:46:51 +0900

    [Feat] redux-persist를 이용한 storage 저장

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import "./index.css";
 import App from "./App";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { dataReducer } from "./data-slice";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+const rootReducer = combineReducers({ data: dataReducer });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: { data: dataReducer },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
+
+export const persistor = persistStore(store);
